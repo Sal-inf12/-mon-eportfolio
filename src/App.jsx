4567dc955a087fb85380dee10e33824eb582bd00
Salah <2169461@cegepmv.ca>
2025-05-08 12:35:29 -0400

    Amélioration Du router 2 path sur 3 fait

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import NavBar from './components/Navbar'
+import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Accueil from './pages/Accueil'
 import Projets from './pages/Projets'
@@ -8,7 +8,8 @@ import Projets from './pages/Projets'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+      
       <Header />
       <NavBar />
       <Routes>
@@ -16,7 +17,7 @@ function App() {
         <Route path="/projets" element={<Projets />} />
       </Routes>
       <Footer />
-    </>
+      </BrowserRouter>
   )
 }
 
