b7cd4518a6011ef146cb82f77b8b2920e42b8e23
Salah <2169461@cegepmv.ca>
2025-05-08 12:15:51 -0400

    ajout de la page API

---
+++ src/FootAPI.jsx
@@ -0,0 +1,14 @@
+import './FootAPI.css'
+
+function FootAPI() {
+  return (
+    <div className="foot-api">
+      <h2>Vidéos de football (API Scorebat)</h2>
+      
+
+      {/* Ici viendront les vidéos avec les boutons et le champ de recherche */}
+    </div>
+  )
+}
+
+export default FootAPI
